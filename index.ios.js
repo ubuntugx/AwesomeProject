b9fbd6540530518e1d5f58e7a71f58ec2ec2e787
Xin Gao <ubuntugx@hotmail.com>
2016-03-28 19:05:57 +0800

    fix bug

--- index.ios.js
+++ index.ios.js
@@ -1,1 +1,48 @@
-import React,{  AppRegistry,  Component,  StyleSheet,  Text,  View,  TabBarIOS,} from 'react-native';import Featured from './components/Featured';   // 精选模块import Search from './components/Search';       // 搜索模块class AwesomeProject extends Component{  constructor(props){    super(props);    this.state={      seletedTab: 'featured',    }  }  render(){    return(       <TabBarIOS>        <TabBarIOS.Item          selected={this.state.seletedTab==='featured'}          onPress={()=>{            this.setState({seletedTab: 'featured'})          }}          systemIcon='featured'>            <Featured />          </TabBarIOS.Item>        <TabBarIOS.Item          selected={this.state.seletedTab==='search'}          onPress={()=>{            this.setState({seletedTab: 'search'})          }}          systemIcon='search'>            <Search />          </TabBarIOS.Item>      </TabBarIOS>    )  }}const styles = StyleSheet.create({})AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+import React,{
+  AppRegistry,
+  Component,
+  StyleSheet,
+  View,
+  TabBarIOS,
+} from 'react-native';
+
+import Featured from './components/Featured';   // 精选模块
+import Search from './components/Search';       // 搜索模块
+
+class demoUbuntugx extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      seletedTab: 'featured',
+    }
+  }
+  render(){
+    return(
+      // 主页面加标签栏，关联的页面分别为两个子页面
+      <TabBarIOS>
+        <TabBarIOS.Item
+          selected={this.state.seletedTab==='featured'}
+          onPress={()=>{
+            this.setState({seletedTab: 'featured'})
+          }}
+          systemIcon='featured'>
+            <Featured />
+          </TabBarIOS.Item>
+        <TabBarIOS.Item
+          selected={this.state.seletedTab==='search'}
+          onPress={()=>{
+            this.setState({seletedTab: 'search'})
+          }}
+          systemIcon='search'>
+            <Search />
+          </TabBarIOS.Item>
+      </TabBarIOS>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+
+})
+
+AppRegistry.registerComponent('demoUbuntugx', () => demoUbuntugx);
